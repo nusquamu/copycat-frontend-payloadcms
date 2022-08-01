5f8958de5adaeb39e4745894f906560a4e7045b9
Ash Simmonds <ashsimmonds@gmail.com>
2022-08-02 07:47:33 +0930

    footer sorta works

--- src/layout/Footer/index.tsx
+++ src/layout/Footer/index.tsx
@@ -1,136 +1,97 @@
-import React from 'react';
-import classes from './index.module.scss';
-import { BlockContainer } from '../BlockContainer';
-import { useGlobals } from '@root/providers/Globals';
-import { Cell, Grid } from '@faceless-ui/css-grid';
-import { FooterMenuLinkType, MenuLink } from './MenuLink';
-import { Hyperlink } from '@components/Hyperlink';
-import { SocialMediaLinks } from '@components/SocialMediaLinks';
-import { Logo } from '@components/Logo';
+import { createStyles, Group } from '@mantine/core';
+// import { IconBrandTwitter, IconBrandYoutube, IconBrandInstagram } from '@tabler/icons';
+// import { MantineLogo } from '@mantine/ds';
+import { Logo } from '../../components/Logo';
 
-export type FooterType = {
-  column1?: FooterMenuLinkType[]
-  column2?: FooterMenuLinkType[]
-}
+const useStyles = createStyles((theme) => ({
+    footer: {
+        marginTop: 120,
+        borderTop: `1px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]
+            }`,
+    },
 
-export const Footer: React.FC = () => {
-  const {
-    footer,
-    meta
-  } = useGlobals();
+    inner: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: `${theme.spacing.md}px ${theme.spacing.md}px`,
 
-  const {
-    column1,
-    column2
-  } = footer || {};
+        [theme.fn.smallerThan('sm')]: {
+            flexDirection: 'column',
+        },
+    },
 
-  const {
-    legalLinks,
-    socialMediaLinks,
-    phone,
-  } = meta || {};
+    links: {
+        [theme.fn.smallerThan('sm')]: {
+            marginTop: theme.spacing.lg,
+            marginBottom: theme.spacing.sm,
+        },
+    },
+}));
 
-  const hasColumn1 = column1 && Array.isArray(column1) && column1.length > 0;
-  const hasColumn2 = column2 && Array.isArray(column2) && column2.length > 0;
-  const hasLegalLinks = legalLinks && Array.isArray(legalLinks) && legalLinks.length > 0;
 
-  return (
-    <div className={classes.footer}>
-      <BlockContainer className={classes.blockContainer}>
-        <div className={classes.content}>
-          <div>
-            <Hyperlink href="/" >
-              <Logo />
-            </Hyperlink>
-          </div>
-          {(hasColumn1 || hasColumn2) && (
-            <Grid>
-              <Cell
-                cols={4}
-                colsM={4}
-                colsS={4}
-                className={classes.column}
-              >
-                {hasColumn1 && column1?.map((item, index) => {
-                  return (
-                    <MenuLink
-                      key={index}
-                      {...item}
-                    />
-                  )
-                })}
-              </Cell>
-              {hasColumn2 && (
-                <Cell
-                  cols={4}
-                  colsM={4}
-                  colsS={4}
-                  className={classes.column}
+export function Footer({ }) {
+    const { classes } = useStyles();
+
+    return (
+        <div className={classes.footer}>
+            <div className={classes.inner}>
+                <Logo />
+
+                <Group className={classes.links}>
+
+                </Group>
+
+
+
+                <Group
+                    spacing="xs"
+                    position="right"
+                    noWrap
                 >
-                  {column2?.map((item, index) => {
-                    return (
-                      <MenuLink
-                        key={index}
-                        {...item}
-                      />
-                    )
-                  })}
-                </Cell>
-              )}
-            </Grid>
-          )}
-          <Grid>
-            <Cell>
-              <div className={classes.contact}>
-                {phone && (
-                  <Hyperlink
-                    href={`tel:+${phone}`}
-                    className={classes.contactLink}
-                    dimOnHover
-                  >
-                    {`Phone: ${phone}`}
-                  </Hyperlink>
-                )}
-                <SocialMediaLinks
-                  className={classes.socialLinks}
-                  links={socialMediaLinks}
-                  useIcons={false}
-                />
-              </div>
-            </Cell>
-          </Grid>
-          <Grid>
-            <Cell>
-              <div className={classes.bottomNav}>
-                <div className={classes.legal}>
-                  <span>
-                    {`©${new Date().getFullYear()} My Website. All rights reserved.`}
-                  </span>
-                  {hasLegalLinks && legalLinks?.map((legalLink, index) => {
-                    const {
-                      link,
-                      link: {
-                        label
-                      }
-                    } = legalLink;
 
-                    return (
-                      <Hyperlink
-                        className={classes.legalLink}
-                        key={index}
-                        linkFromCMS={link}
-                        dimOnHover
-                      >
-                        {label}
-                      </Hyperlink>
-                    )
-                  })}
-                </div>
-              </div>
-            </Cell>
-          </Grid>
+
+                    <a href="https://twitter.com/BlueDwarfSpace">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            className="fill-neutral"
+                        >
+                            <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z">
+                            </path>
+                        </svg>
+                    </a>
+                    <a href="https://www.linkedin.com/company/bluedwarfspace">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="28"
+                            height="28"
+                            viewBox="0 0 32 32"
+                            className="fill-neutral"
+                        >
+                            <path d="M 8.6425781 4 C 7.1835781 4 6 5.181625 6 6.640625 C 6 8.099625 7.182625 9.3085938 8.640625 9.3085938 C 10.098625 9.3085938 11.283203 8.099625 11.283203 6.640625 C 11.283203 5.182625 10.101578 4 8.6425781 4 z M 21.535156 11 C 19.316156 11 18.0465 12.160453 17.4375 13.314453 L 17.373047 13.314453 L 17.373047 11.310547 L 13 11.310547 L 13 26 L 17.556641 26 L 17.556641 18.728516 C 17.556641 16.812516 17.701266 14.960938 20.072266 14.960938 C 22.409266 14.960937 22.443359 17.145609 22.443359 18.849609 L 22.443359 26 L 26.994141 26 L 27 26 L 27 17.931641 C 27 13.983641 26.151156 11 21.535156 11 z M 6.3632812 11.310547 L 6.3632812 26 L 10.923828 26 L 10.923828 11.310547 L 6.3632812 11.310547 z">
+                            </path>
+                        </svg>
+                    </a>
+                    <a href="https://www.facebook.com/bluedwarfspace">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            className="fill-neutral"
+                        >
+                            <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z">
+                            </path>
+                        </svg>
+                    </a>
+
+
+
+                </Group>
+            </div>
         </div>
-      </BlockContainer>
-    </div>
-  )
+    );
 }
